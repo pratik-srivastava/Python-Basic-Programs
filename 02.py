414c85435420e57fc15909001196b49cb864c539
pratik-srivastava <86921160+pratik-srivastava@users.noreply.github.com>
2021-07-09 02:17:06 +0530

    Add files via upload
    To find maximum numbers among two numbers

---
+++ 02.py
@@ -0,0 +1,10 @@
+a=int(input("Input the first number:"))
+b=int(input("Input the second number: "))
+if a>b:
+    print(f"{a} is greater than {b}")
+
+elif a<b:
+    print(f"{a} is lesser than {b}")
+
+else:
+    print("Both are equal")
